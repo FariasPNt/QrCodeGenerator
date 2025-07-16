ab943c2555616665f48bd4aec0f115cbe67ff5bf
FariasPNt <farias_neto@outlook.com>
2025-07-15 21:28:27 -0300

    docker e aws

--- src/main/java/com/AntonioFarias/ProjetoQrCode/controller/QrCodeController.java
+++ src/main/java/com/AntonioFarias/ProjetoQrCode/controller/QrCodeController.java
@@ -3,6 +3,7 @@ package com.AntonioFarias.ProjetoQrCode.controller;
 
 import com.AntonioFarias.ProjetoQrCode.dto.QrCodeGenerateRequest;
 import com.AntonioFarias.ProjetoQrCode.dto.QrCodeGenerateResponse;
+import com.AntonioFarias.ProjetoQrCode.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +11,19 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeGeneratorService) {
+        this.qrCodeGeneratorService = qrCodeGeneratorService;
+    }
 
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request){
-        return null;
+        try{
+            QrCodeGenerateResponse response =  this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception e){
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
